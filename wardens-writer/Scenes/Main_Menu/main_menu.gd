e7acd0ae9b0c71d4b59e0f8e2535d209012812b6
Jeffrey <W0438905@nscc.ca>
2025-03-04 11:12:19 -0400

    Moved Background, Added File Pathing, Added Beta Write Menu
    Background is its own node to apply themes easier.
    
    Var button_route renamed to button_name.
    
    Main Menu Button now changes packed scenes based on button_name var.
    
    Made beginning of Write scene.

--- wardens-writer/Scenes/Main_Menu/main_menu.gd
+++ wardens-writer/Scenes/Main_Menu/main_menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
+@onready var write_button: TextureButton = $CanvasLayer/MarginContainer/VBoxContainer/Write
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	write_button.scale = Vector2(1.5, 1.5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- wardens-writer/Scenes/Main_Menu_Button/main_menu_button.gd
+++ wardens-writer/Scenes/Main_Menu_Button/main_menu_button.gd
@@ -1,16 +1,17 @@
 extends TextureButton
 
+# Temporary resizing 
 const HOVER_SCALE: Vector2 = Vector2(1.1, 1.1)
 const DEFAULT_SCALE: Vector2 = Vector2(1.0, 1.0)
 
-@export var button_route: String = ""
+@export var button_name: String = "" # 
 
 @onready var name_label: Label = $NameLabel
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	name_label.text = button_route
-	#_level_scene = load("res://Scenes/Level/level%s.tscn" % level_number)
+	name_label.text = button_name.capitalize()
 	
 	# make two exported vars, one for the display name and another for the path
 	# or format one to fill both
@@ -22,7 +23,18 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	pass #get_tree().change_scene_to_packed(_level_scene)
+	if button_name == "exit":
+		get_tree().quit() # Exits the program
+	elif button_name == "main_menu":
+		# .capitalize() turns main_menu into Main Menu, which is not Main_Menu like I need it to be.
+		# This is the fix
+		var button_route: PackedScene = load("res://Scenes/Main_Menu/main_menu.tscn")
+		get_tree().change_scene_to_packed(button_route)
+	else:
+		var button_route: PackedScene = load("res://Scenes/" + button_name.capitalize() + "/" + button_name + ".tscn")
+		get_tree().change_scene_to_packed(button_route)
+		#print("Button Clicked: " + button_name) # Debug
+		#print("res://Scenes/" + button_name.capitalize() + "/" + button_name + ".tscn") # Debug
 
 
 func _on_mouse_entered() -> void:
