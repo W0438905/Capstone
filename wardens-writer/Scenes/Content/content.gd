153e4c94e966e95cd0312e9eb27e492466f0bc94
Jeffrey <W0438905@nscc.ca>
2025-03-31 23:24:38 -0300

    Added Update Times to Content Page Saving
    + Altered content query to also update the updatedAt field.
    + Added query in Content page to also update the current story's updatedAt field.

--- wardens-writer/Scenes/Content/content.gd
+++ wardens-writer/Scenes/Content/content.gd
@@ -8,6 +8,7 @@ var info: Dictionary
 var id: int
 var table: String
 var pk: String
+var story_id: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,19 +25,19 @@ func chapter_or_note() -> void:
 	if cn == "chapter":
 		info = StoryManager.get_chapter_info()
 		id = info["chapter_id"]
-		title_line.text = info["title"]
-		content_edit.text = info["content"]
 		table = "CHAPTERS"
 		pk = "chapterId"
 		#print("chapter info in content:")
-		print(id)
+		#print(id)
+		
 	elif cn == "note":
 		info = StoryManager.get_note_info()
 		id = info["note_id"]
-		title_line.text = info["title"]
-		content_edit.text = info["content"]
 		table = "NOTES"
 		pk = "noteId"
+		#print("note info in content:")
+		#print(id)
+		
 	else:
 		pass # write an error to the content box and disable the save button
 
@@ -46,17 +47,32 @@ func _on_back_button_pressed() -> void:
 
 
 func _on_save_button_pressed() -> void:
+	story_id = info["story_id"]
+	title_line.text = info["title"]
+	content_edit.text = info["content"]
+	
 	var title: String = title_line.text
 	var content: String = content_edit.text
+	var date: String = StoryManager.get_date()
 	
-	var query: String = """
+	# Write content changes to respective table
+	var content_query: String = """
 		UPDATE %s
-		SET title = ?, content = ?
+		SET title = ?, content = ?, updatedAt = ?
 		WHERE %s = ?
 	""" % [table, pk]
-	var params: Array = [title, content, id]
+	var content_params: Array = [title, content, date, id]
 	
-	DatabaseManager.db.query_with_bindings(query, params)
+	# Update when the story was updated
+	var story_query: String = """
+		UPDATE Stories
+		SET updatedAt = ?
+		WHERE storyId = ?
+	"""
+	var story_params: Array = [date, story_id]
+	
+	DatabaseManager.db.query_with_bindings(content_query, content_params)
+	DatabaseManager.db.query_with_bindings(story_query, story_params)
 
 
 func _on_timer_timeout() -> void:
